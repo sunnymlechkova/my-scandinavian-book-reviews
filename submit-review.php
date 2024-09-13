839a360c63d155446134d0f355bfd7e8cf98fb34
sunnymlechkova <malinkakalinka13579@gmail.com>
2024-09-13 15:36:37 +0300

    Update submit-review.php

--- submit-review.php
+++ submit-review.php
@@ -5,8 +5,6 @@ $bookTitle = htmlspecialchars($_POST['bookTitle']);
 $author = htmlspecialchars($_POST['author']);
 $review = htmlspecialchars($_POST['review']);
 
-```
-// Create the review entry
 $reviewEntry = $bookTitle . '|' . $author . '|' . $review . "\\n";
 
 // Save the review into the reviews.txt file
@@ -15,8 +13,3 @@ file_put_contents('reviews.txt', $reviewEntry, FILE_APPEND);
 // Redirect back to the main page
 header('Location: index.php');
 exit();
-
-```
-
-}
-?>
